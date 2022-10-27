695bc833ee6262ab9e61335215ed2fbd243c25cd
Can <hcdayan@hotmail.com>
2022-10-27 10:35:18 +0200

    Add colors. Add color picker.

---
+++ Task Manager/View/AddNewTask.swift
@@ -0,0 +1,71 @@
+//
+//  AddNewTask.swift
+//  Task Manager
+//
+//  Created by Can on 26.10.2022.
+//
+
+import SwiftUI
+
+struct AddNewTask: View {
+    @EnvironmentObject var taskViewModel: TaskViewModel
+    @Environment(\.self) var env
+    
+    var body: some View {
+        VStack(spacing: 12.0) {
+            Text("Edit Task")
+                .font(.title3.bold())
+                .frame(maxWidth: .infinity)
+                .overlay(alignment: .leading) {
+                    Button {
+                        env.dismiss()
+                    } label: {
+                        Image(systemName: "arrow.left")
+                            .font(.title3)
+                            .foregroundColor(.black)
+                    }
+                }
+            
+            VStack(alignment: .leading, spacing: 12.0) {
+                Text("Task Color")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                
+                let sampleCardColors: [String] = ["Yellow", "Green", "Blue", "Purple", "Red", "Orange"]
+                
+                HStack(spacing: 15.0) {
+                    ForEach(sampleCardColors, id: \.self) { color in
+                        Circle()
+                            .fill(Color(color))
+                            .frame(width: 25.0, height: 25.0)
+                            .background {
+                                if taskViewModel.taskColor == color {
+                                    Circle()
+                                        .strokeBorder(.gray)
+                                        .padding(-3.0)
+                                }
+                            }
+                            .contentShape(Circle())
+                            .onTapGesture {
+                                taskViewModel.taskColor = color
+                            }
+                    }
+                }
+                .padding(.top, 10.0)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.top, 30.0)
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding()
+        
+         
+    }
+}
+
+struct AddNewTask_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewTask()
+            .environmentObject(TaskViewModel())
+    }
+}
